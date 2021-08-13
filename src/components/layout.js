7affaea05367293cb4332534e79654a265dd03c3
lee abell <lee.abell@finclusionsystems.com>
2021-08-13 20:49:11 +0100

    first commit

---
+++ src/components/layout.js
@@ -0,0 +1,55 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
+
+import Header from './header';
+import './layout.css';
+import { SingleModal } from './modal';
+import { useSingleModal } from '../contexts/SingleModalContext';
+
+const Layout = ({ children }) => {
+  const { show, toggle, content } = useSingleModal();
+
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
+        }}
+      >
+        <main>{children}</main>
+        <footer>© {new Date().getFullYear()}, TekMi</footer>
+        <SingleModal isOpen={show} toggle={() => toggle()} unmountOnClose={false}>
+          {content}
+        </SingleModal>
+      </div>
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
---
+++ src/pages/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import SEO from '../components/seo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, ModalBody, ModalFooter, ModalHeader, ButtonGroup } from 'reactstrap';
+import { useSingleModal } from '../contexts/SingleModalContext';
+import { Link } from 'gatsby';
+
+const IndexPage = () => {
+  const { toggle, close, setContent } = useSingleModal();
+
+  const toggleModalWithContent = (cont) => {
+    setContent(cont);
+    toggle();
+  };
+
+  return (
+    <>
+      <SEO title="Home" />
+      <ButtonGroup>
+        <Button color="info" onClick={() => toggleModalWithContent(() => 'adam')}>
+          Open Modal
+        </Button>
+        <Button color="warning" onClick={() => toggleModalWithContent(() => 11)}>
+          Open Number Modal
+        </Button>
+        <Button color="success" onClick={() => toggleModalWithContent(() => [11, 22, 33])}>
+          Open Array Modal
+        </Button>
+        <Button
+          color="danger"
+          onClick={() =>
+            toggleModalWithContent(() => {
+              return (
+                <>
+                  <ModalHeader>Modal title</ModalHeader>
+                  <ModalBody>Another Test</ModalBody>
+                  <ModalFooter>
+                    <Button color="primary" onClick={() => close()}>
+                      Do Something
+                    </Button>{' '}
+                    <Button color="secondary" onClick={() => close()}>
+                      Cancel
+                    </Button>
+                  </ModalFooter>
+                </>
+              );
+            })
+          }
+        >
+          Open JSX Modal
+        </Button>
+      </ButtonGroup>
+      <br />
+      <Link to="/page-2">Go To Second page</Link>
+    </>
+  );
+};
+
+export default IndexPage;
